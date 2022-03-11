e62fcbd781231eb884224bc44dc31cbae033a430
Tanmay Padhi <48876183+tanmaypadhi08@users.noreply.github.com>
2022-03-11 22:19:13 +0530

    Update main.c

--- 1_Remote_Keyless_Entry/3_Implementation/main.c
+++ 1_Remote_Keyless_Entry/3_Implementation/main.c
@@ -1,17 +1,31 @@
+/**
+ * @file main.c
+ * @author Group 65
+ * @brief This projects demonstrates Remote keyless Entry with STM32 as microcontroller and is working as a unidirectional system 
+ * @version 0.1
+ * @date 2022-03-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <stdio.h>
 #include "MyStm32f407xx.h"
 #include<stdbool.h>
 
 
 #define BTN_PRESSED ENABLE
-
-int encrypt()
+/**
+ * @brief This function runs successfully if encrypt is true
+ * 
+ * @return flag 
+ */
+int encrypt() // function made by K Mohammad Habeeb 
 
 { 
    bool flag = false;
    const int a = 123;
    const int b = 234;
-   int result = a * b;
+   int result = a * b; // algorithm for encryption
    if(result==28782)
    {
 
@@ -24,49 +38,69 @@ int encrypt()
 
    return flag;
 }
+   /**
+    * @brief The delay function is declared for configuring the STM32 push button
+    * 
+    */
    void delay(void)
    {
       for (uint32_t i = 0; i < 5000000; i++);
    }
-   void unlock(void)
+   /**
+    * @brief Here we have made a function for alarm status, where all the LEDs are in off condition
+    * 
+    */
+   void unlock (void)    // Code By Tanmay
    {
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0);
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0);
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0);
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0);
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 0); // Green LED
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 0); // Orange LED
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 0); // Red LED
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 0); // Blue LED
 
    }
-   void lock(void)
+   /**
+    * @brief Here we have made a function for window, where all the LEDs are in on condition
+    * 
+    */
+   void lock (void)    // Code by Sharath
    {
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 1);
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 1);
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 1);
-      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 1);
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_12, 1);  // Green LED
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_13, 1);  // Orange LED
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_14, 1);  // Red LED
+      GPIO_WriteToOutputPin(GPIOD, GPIO_PIN_NO_15, 1);   // Blue LED
 
    }
-   void Alarm_Activation_Deactivation(void)
+   /**
+    * @brief Here we have made a function for car battery info, where all the LEDs are turned ON in clockwise sequence
+    * 
+    */
+   void alarm_activation_deactivation(void) // The logic for this function was given by Tanmay 
    {
-      
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
       delay();
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12); // Green LED
       delay();
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13); // Orange LED
       delay();
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);  // RED LED
+      delay();
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);  // Blue LED
       delay();
 
    }
-   void Approach_light(void)
+   /**
+    * @brief Here we have made a function for door status, where all the LEDs are turned ON in anti-clockwise sequence
+    * 
+    */
+   void Approach_Light(void) // The logic for this function was given by Sharath
    {
-     
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15);
       delay();
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14);
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_15); // Blue LED
       delay();
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_14); // Red LED
       delay();
-      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13); // Orange LED
+      delay();
+      GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);  // Green LED
       delay();
       
    }
@@ -75,8 +109,8 @@ int encrypt()
       int count = 0;
       GPIO_Handle_t GpioLed, GPIOBtn, GpioLed1, GpioLed2, GpioLed3;
 
-      GpioLed.pGPIOx = GPIOD;
-      GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+      GpioLed.pGPIOx = GPIOD;                                    // The declaration of the pins is given by Anurag Tiwari
+      GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;     // PIN D12 is Green LED
       GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
       GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
       GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
@@ -84,7 +118,7 @@ int encrypt()
       GPIO_Init(&GpioLed);
 
       GpioLed1.pGPIOx = GPIOD;
-      GpioLed1.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+      GpioLed1.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;      // PIN D13 is Orange LED
       GpioLed1.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
       GpioLed1.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
       GpioLed1.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
@@ -92,7 +126,7 @@ int encrypt()
       GPIO_Init(&GpioLed1);
 
       GpioLed2.pGPIOx = GPIOD;
-      GpioLed2.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+      GpioLed2.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;      // PIN D14 is Red LED
       GpioLed2.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
       GpioLed2.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
       GpioLed2.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
@@ -100,7 +134,7 @@ int encrypt()
       GPIO_Init(&GpioLed2);
 
       GpioLed3.pGPIOx = GPIOD;
-      GpioLed3.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+      GpioLed3.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;     // PIN D15 is Blue LED
       GpioLed3.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
       GpioLed3.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
       GpioLed3.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
@@ -108,7 +142,7 @@ int encrypt()
       GPIO_Init(&GpioLed3);
 
       GPIOBtn.pGPIOx = GPIOA;
-      GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+      GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;    // PIN A0 is Blue Switch
       GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
       GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
       GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
@@ -120,12 +154,12 @@ int encrypt()
 
          while (1)
          {
-            if (GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == BTN_PRESSED)
+            if (GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0) == BTN_PRESSED)   // The function was dicussed and implemented by whole group
             {
                delay();
-               count = count + 1;
+               count = count + 1;    // count is incremented when push button is pressed by the user
             }
-            if (count == 3)
+            if (count == 3)  
             {
                lock();
             }
@@ -136,11 +170,11 @@ int encrypt()
             else if (count==18)
             {
 
-               Alarm_Activation_Deactivation();
+               alarm_activation_deactivation();
             }
             else if (count==27)
             {
-               Approach_light();
+               Approach_Light();
             }
          }
       }
